d732acd2b4ffbc3a30d07c0f93f0f3b005b5283c
Vinod Patil <developer.vinod.patil@gmail.com>
2022-05-29 21:10:08 +0530

    functionality to persist the drawn paths is added by saving all paths in a Array mPaths

--- app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -15,7 +15,7 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs){
     private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas: Canvas? = null
-    private var mPaths = ArrayList<CustomPath>()
+    private val mPaths = ArrayList<CustomPath>()
 
     init{
         setUpDrawing()
@@ -40,6 +40,13 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs){
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!,0f,0f, mCanvasPaint)
+
+        for(path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+
         if(!mDrawPath!!.isEmpty){
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color
@@ -70,6 +77,7 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs){
             }
 
             MotionEvent.ACTION_UP ->{
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
             }
             else -> return false
